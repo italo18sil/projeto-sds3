fa53b7d41e17d9f60c727654f904b0b9cf4817f2
italosassuncao <italo18sil@gmail.com>
2024-04-12 15:01:02 -0300

    Page update

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/italo18sil" target="_blank" rel="noreferrer">Italo Silva</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light">Aplicativo desenvolvido por <a href="https://github.com/italosassuncao" target="_blank" rel="noreferrer">Italo Assunção</a></p>
+                <p className="text-light">LinkedIn<a href="https://www.linkedin.com/in/italosassuncao/" target="_blank" rel="noreferrer"></a></p>
+                <p className="text-light"><small>All rights reserved ©<strong></strong>
             </div>
         </footer>
 
