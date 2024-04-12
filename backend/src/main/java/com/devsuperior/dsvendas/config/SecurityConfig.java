79ef7d35795f1fd72e3c039c04972d2bedd0f756
italosassuncao <italo18sil@gmail.com>
2024-04-12 15:29:16 -0300

    Update project

--- backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/config/SecurityConfig.java
@@ -3,33 +3,36 @@ package com.devsuperior.dsvendas.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	@Autowired
 	private Environment env;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable();
+            http.headers(withDefaults());
 		}
-		
-		http.cors().and().csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+
+        http.cors(cors -> cors.csrf(csrf -> csrf.disable()));
+        http.sessionManagement(withDefaults());
+        http.authorizeRequests(withDefaults());
+        return http.build();
 	}
 
 	@Bean
